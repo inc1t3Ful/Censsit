51d27fb1fdedef248cd526e92e9c9098447c85ce
Dianna K <dikwan@ucsc.edu>
2018-01-20 16:48:54 -0800

    blackout

--- content.js
+++ content.js
@@ -1,8 +1,28 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
+console.log("content.js");
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if (request.todo == "censorBadWord"){
-		var word = request.clickedWord;
-		$('.commentarea .sitetable .usertext p').text('bad word');
-	}
-})
+function censor(){
+    var badWords = ['fuck', 'hell', 'shit', 'anus','ass','credit'];
+    var x = document.getElementsByClassName("md");
+    var i;
+    for (i = 0; i<x.length; i++) {
+        var badword = false;
+        //x[i].style.backgroundColor = "black";
+        var comment = x[i].innerHTML.split(" ");
+        for (var j = 0; j< comment.length; j++) {
+             console.log(comment[j]);
+            for (var k = 0; k<badWords.length; k++){
+                if (comment[j].includes(badWords[k])){
+                    badword = true;
+                    break;
+                }   
+            }
+        }
+        if (badword){
+            x[i].style.backgroundColor = "black";
+            //x[i].removeAttribute;
+        }
+    }
+    console.log("what");
+}
+censor();
