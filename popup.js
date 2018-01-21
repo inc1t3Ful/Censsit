0104bc033710409c0df4fbd4d3ba106e5178abb0
Brian Liu <Brian@dhcp-169-233-155-116.cruznet.ucsc.edu>
2018-01-21 01:57:00 -0800

    made giant array of bad words

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
 $(function(){
 	var word;
-	$('#bad_word').change(function(){
-		if (this.checked){
-			word = $(this).val();
-		}
+	$('#bad_word').on('input', function(){
+		word = $(this).val()
 	});
 	$('#btnCensor').click(function(){
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
