650e550210d85ff83119515261f10c85059da03e
Dianna K <dikwan@ucsc.edu>
2018-01-20 17:33:46 -0800

    Replaces offensive comment with preset text.

--- content.js
+++ content.js
@@ -1,16 +1,23 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
 console.log("content.js");
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+ 	if (request.todo == "censorBadWord"){
+ 		var word = request.clickedWord;
+ 		$('.commentarea .sitetable .usertext p').text('bad word');
+ 	}
+ })
+
 function censor(){
-    var badWords = ['fuck', 'hell', 'shit', 'anus','ass','credit'];
+    var badWords = ['fuck'];
     var x = document.getElementsByClassName("md");
-    var i;
-    for (i = 0; i<x.length; i++) {
+
+    for (var i = 0; i<x.length; i++) {
         var badword = false;
         //x[i].style.backgroundColor = "black";
-        var comment = x[i].innerHTML.split(" ");
+        var comment = x[i].innerHTML.toLowerCase().split(" ");
         for (var j = 0; j< comment.length; j++) {
-             console.log(comment[j]);
+             
             for (var k = 0; k<badWords.length; k++){
                 if (comment[j].includes(badWords[k])){
                     badword = true;
@@ -19,8 +26,9 @@ function censor(){
             }
         }
         if (badword){
-            x[i].style.backgroundColor = "black";
-            //x[i].removeAttribute;
+            //x[i].style.backgroundColor = "black";
+            //x.removeChild(x[i]);
+            x[i].innerHTML = "This comment contains offensive language and has been filtered.";
         }
     }
     console.log("what");
