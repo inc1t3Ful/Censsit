2bbd210e54b3cc489d97b4cd44ca7747b184681b
Brian Liu <Brian@Brians-MacBook-Air.local>
2018-01-21 10:31:39 -0800

    new way of parsing through html

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,11 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if (request.todo == "showPageAction")
-    {
-        chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
-            chrome.pageAction.show(tabs[0].id);
-        });
-    }
-});
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+// 	chrome.pageAction.show(tabs[0].id);
+// });
+
+chrome.runtime.onMessage.addListener(function(request. sender, sendResponse){
+	if (request.todo == "showPageAction"){
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+			chrome.pageAction.show(tabs[0].id);
+		});
+	}
+})
